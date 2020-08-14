27751873861e5d31c42b63dd9492231b115f96a7
UtkuMert <65029733+UtkuMert@users.noreply.github.com>
2020-08-14 22:43:04 +0300

    UygulamaKod
    Uygulama Kodlari

---
+++ Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace G171210051_Proje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.doktorIdLabel = new System.Windows.Forms.Label();
+            this.doktorIdTextBox = new System.Windows.Forms.TextBox();
+            this.doktorUcretLabel = new System.Windows.Forms.Label();
+            this.doktorUcretTextBox = new System.Windows.Forms.TextBox();
+            this.doktorSifreLabel = new System.Windows.Forms.Label();
+            this.doktorSifreTextBox = new System.Windows.Forms.TextBox();
+            this.doktorBransLabel = new System.Windows.Forms.Label();
+            this.doktorBransTextBox = new System.Windows.Forms.TextBox();
+            this.doktorEkleButton = new System.Windows.Forms.Button();
+            this.sekreterSifreLabel = new System.Windows.Forms.Label();
+            this.sekreterSifreTextBox = new System.Windows.Forms.TextBox();
+            this.sekreterSifreButton = new System.Windows.Forms.Button();
+            this.silinecekHastaLabel = new System.Windows.Forms.Label();
+            this.silinecekHastaTextBox = new System.Windows.Forms.TextBox();
+            this.silinecekHastaButton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.hastaAramaButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // doktorIdLabel
+            // 
+            this.doktorIdLabel.AutoSize = true;
+            this.doktorIdLabel.Location = new System.Drawing.Point(12, 38);
+            this.doktorIdLabel.Name = "doktorIdLabel";
+            this.doktorIdLabel.Size = new System.Drawing.Size(57, 13);
+            this.doktorIdLabel.TabIndex = 0;
+            this.doktorIdLabel.Text = "Doktor Id :";
+            // 
+            // doktorIdTextBox
+            // 
+            this.doktorIdTextBox.Location = new System.Drawing.Point(109, 38);
+            this.doktorIdTextBox.Name = "doktorIdTextBox";
+            this.doktorIdTextBox.Size = new System.Drawing.Size(100, 20);
+            this.doktorIdTextBox.TabIndex = 1;
+            // 
+            // doktorUcretLabel
+            // 
+            this.doktorUcretLabel.AutoSize = true;
+            this.doktorUcretLabel.Location = new System.Drawing.Point(12, 88);
+            this.doktorUcretLabel.Name = "doktorUcretLabel";
+            this.doktorUcretLabel.Size = new System.Drawing.Size(74, 13);
+            this.doktorUcretLabel.TabIndex = 2;
+            this.doktorUcretLabel.Text = "Doktor Ucret :";
+            // 
+            // doktorUcretTextBox
+            // 
+            this.doktorUcretTextBox.Location = new System.Drawing.Point(109, 88);
+            this.doktorUcretTextBox.Name = "doktorUcretTextBox";
+            this.doktorUcretTextBox.Size = new System.Drawing.Size(100, 20);
+            this.doktorUcretTextBox.TabIndex = 3;
+            // 
+            // doktorSifreLabel
+            // 
+            this.doktorSifreLabel.AutoSize = true;
+            this.doktorSifreLabel.Location = new System.Drawing.Point(12, 133);
+            this.doktorSifreLabel.Name = "doktorSifreLabel";
+            this.doktorSifreLabel.Size = new System.Drawing.Size(69, 13);
+            this.doktorSifreLabel.TabIndex = 4;
+            this.doktorSifreLabel.Text = "Doktor Sifre :";
+            // 
+            // doktorSifreTextBox
+            // 
+            this.doktorSifreTextBox.Location = new System.Drawing.Point(109, 133);
+            this.doktorSifreTextBox.Name = "doktorSifreTextBox";
+            this.doktorSifreTextBox.Size = new System.Drawing.Size(100, 20);
+            this.doktorSifreTextBox.TabIndex = 5;
+            // 
+            // doktorBransLabel
+            // 
+            this.doktorBransLabel.AutoSize = true;
+            this.doktorBransLabel.Location = new System.Drawing.Point(12, 181);
+            this.doktorBransLabel.Name = "doktorBransLabel";
+            this.doktorBransLabel.Size = new System.Drawing.Size(75, 13);
+            this.doktorBransLabel.TabIndex = 6;
+            this.doktorBransLabel.Text = "Doktor Brans :";
+            // 
+            // doktorBransTextBox
+            // 
+            this.doktorBransTextBox.Location = new System.Drawing.Point(109, 181);
+            this.doktorBransTextBox.Name = "doktorBransTextBox";
+            this.doktorBransTextBox.Size = new System.Drawing.Size(100, 20);
+            this.doktorBransTextBox.TabIndex = 7;
+            // 
+            // doktorEkleButton
+            // 
+            this.doktorEkleButton.Location = new System.Drawing.Point(31, 221);
+            this.doktorEkleButton.Name = "doktorEkleButton";
+            this.doktorEkleButton.Size = new System.Drawing.Size(178, 81);
+            this.doktorEkleButton.TabIndex = 8;
+            this.doktorEkleButton.Text = "Yeni Doktor Ekle";
+            this.doktorEkleButton.UseVisualStyleBackColor = true;
+            this.doktorEkleButton.Click += new System.EventHandler(this.doktorEkleButton_Click);
+            // 
+            // sekreterSifreLabel
+            // 
+            this.sekreterSifreLabel.AutoSize = true;
+            this.sekreterSifreLabel.Location = new System.Drawing.Point(12, 362);
+            this.sekreterSifreLabel.Name = "sekreterSifreLabel";
+            this.sekreterSifreLabel.Size = new System.Drawing.Size(108, 13);
+            this.sekreterSifreLabel.TabIndex = 9;
+            this.sekreterSifreLabel.Text = "Yeni Sekreter Sifresi :";
+            // 
+            // sekreterSifreTextBox
+            // 
+            this.sekreterSifreTextBox.Location = new System.Drawing.Point(135, 359);
+            this.sekreterSifreTextBox.Name = "sekreterSifreTextBox";
+            this.sekreterSifreTextBox.Size = new System.Drawing.Size(100, 20);
+            this.sekreterSifreTextBox.TabIndex = 10;
+            // 
+            // sekreterSifreButton
+            // 
+            this.sekreterSifreButton.Location = new System.Drawing.Point(88, 400);
+            this.sekreterSifreButton.Name = "sekreterSifreButton";
+            this.sekreterSifreButton.Size = new System.Drawing.Size(183, 38);
+            this.sekreterSifreButton.TabIndex = 11;
+            this.sekreterSifreButton.Text = "SIFRE DEGIS";
+            this.sekreterSifreButton.UseVisualStyleBackColor = true;
+            this.sekreterSifreButton.Click += new System.EventHandler(this.sekreterSifreButton_Click);
+            // 
+            // silinecekHastaLabel
+            // 
+            this.silinecekHastaLabel.AutoSize = true;
+            this.silinecekHastaLabel.Location = new System.Drawing.Point(524, 38);
+            this.silinecekHastaLabel.Name = "silinecekHastaLabel";
+            this.silinecekHastaLabel.Size = new System.Drawing.Size(103, 13);
+            this.silinecekHastaLabel.TabIndex = 12;
+            this.silinecekHastaLabel.Text = "Silenecek Hasta Id :";
+            // 
+            // silinecekHastaTextBox
+            // 
+            this.silinecekHastaTextBox.Location = new System.Drawing.Point(633, 35);
+            this.silinecekHastaTextBox.Name = "silinecekHastaTextBox";
+            this.silinecekHastaTextBox.Size = new System.Drawing.Size(100, 20);
+            this.silinecekHastaTextBox.TabIndex = 13;
+            // 
+            // silinecekHastaButton
+            // 
+            this.silinecekHastaButton.Location = new System.Drawing.Point(616, 78);
+            this.silinecekHastaButton.Name = "silinecekHastaButton";
+            this.silinecekHastaButton.Size = new System.Drawing.Size(117, 30);
+            this.silinecekHastaButton.TabIndex = 14;
+            this.silinecekHastaButton.Text = "HASTAYI SIL";
+            this.silinecekHastaButton.UseVisualStyleBackColor = true;
+            this.silinecekHastaButton.Click += new System.EventHandler(this.silinecekHastaButton_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(368, 181);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(242, 249);
+            this.richTextBox1.TabIndex = 18;
+            this.richTextBox1.Text = "";
+            // 
+            // hastaAramaButton
+            // 
+            this.hastaAramaButton.Location = new System.Drawing.Point(633, 198);
+            this.hastaAramaButton.Name = "hastaAramaButton";
+            this.hastaAramaButton.Size = new System.Drawing.Size(129, 46);
+            this.hastaAramaButton.TabIndex = 19;
+            this.hastaAramaButton.Text = "HASTA ARAMA";
+            this.hastaAramaButton.UseVisualStyleBackColor = true;
+            this.hastaAramaButton.Click += new System.EventHandler(this.hastaAramaButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.hastaAramaButton);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.silinecekHastaButton);
+            this.Controls.Add(this.silinecekHastaTextBox);
+            this.Controls.Add(this.silinecekHastaLabel);
+            this.Controls.Add(this.sekreterSifreButton);
+            this.Controls.Add(this.sekreterSifreTextBox);
+            this.Controls.Add(this.sekreterSifreLabel);
+            this.Controls.Add(this.doktorEkleButton);
+            this.Controls.Add(this.doktorBransTextBox);
+            this.Controls.Add(this.doktorBransLabel);
+            this.Controls.Add(this.doktorSifreTextBox);
+            this.Controls.Add(this.doktorSifreLabel);
+            this.Controls.Add(this.doktorUcretTextBox);
+            this.Controls.Add(this.doktorUcretLabel);
+            this.Controls.Add(this.doktorIdTextBox);
+            this.Controls.Add(this.doktorIdLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label doktorIdLabel;
+        private System.Windows.Forms.TextBox doktorIdTextBox;
+        private System.Windows.Forms.Label doktorUcretLabel;
+        private System.Windows.Forms.TextBox doktorUcretTextBox;
+        private System.Windows.Forms.Label doktorSifreLabel;
+        private System.Windows.Forms.TextBox doktorSifreTextBox;
+        private System.Windows.Forms.Label doktorBransLabel;
+        private System.Windows.Forms.TextBox doktorBransTextBox;
+        private System.Windows.Forms.Button doktorEkleButton;
+        private System.Windows.Forms.Label sekreterSifreLabel;
+        private System.Windows.Forms.TextBox sekreterSifreTextBox;
+        private System.Windows.Forms.Button sekreterSifreButton;
+        private System.Windows.Forms.Label silinecekHastaLabel;
+        private System.Windows.Forms.TextBox silinecekHastaTextBox;
+        private System.Windows.Forms.Button silinecekHastaButton;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button hastaAramaButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace G171210051_Proje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void databaseDec(string komut)
+        {
+
+            try
+            {
+                string connstring = "Server=127.0.0.1; Port=5432; User Id=postgres; Password=e.1.2.3.; Database=HOSPITAL;";
+                NpgsqlConnection connection = new NpgsqlConnection(connstring);
+                connection.Open();
+
+
+                NpgsqlCommand command = new NpgsqlCommand(komut, connection);
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (Exception msg)
+            {
+                MessageBox.Show(msg.ToString());
+                throw;
+            }
+        }
+
+        private void doktorEkleButton_Click(object sender, EventArgs e)
+        {
+            string komut = " SELECT *from doktorekle(" + doktorIdTextBox.Text + "," + doktorUcretTextBox.Text + "," + doktorSifreTextBox.Text + "," + doktorBransTextBox.Text + ");";
+            databaseDec(komut);
+        }
+
+        private void sekreterSifreButton_Click(object sender, EventArgs e)
+        {
+            string komut = " SELECT *from sifreGuncelle(" + sekreterSifreTextBox.Text + ");";
+            databaseDec(komut);
+        }
+
+        private void silinecekHastaButton_Click(object sender, EventArgs e)
+        {
+            string komut = " SELECT *from hastaSil(" + silinecekHastaTextBox.Text + ");";
+            databaseDec(komut);
+        }
+
+        private void hastaAramaButton_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            try
+            {
+                string connstring = "Server=127.0.0.1; Port=5432; User Id=postgres; Password=e.1.2.3.; Database=HOSPITAL;";
+                NpgsqlConnection connection = new NpgsqlConnection(connstring);
+                connection.Open();
+                string komut = "SELECT * FROM " + '"' + "public" + '"' + '.' + '"' + "HASTA" + '"';
+                NpgsqlCommand command = new NpgsqlCommand(komut, connection);
+                NpgsqlDataReader dataReader = command.ExecuteReader();
+                for (int i = 0; dataReader.Read(); i++)
+                {
+                    richTextBox1.Text += "Hasta Adi: " + dataReader[1].ToString() + "  Hasta Soyadi: " + dataReader[2].ToString() +"\n\n";
+                }
+                connection.Close();
+            }
+            catch (Exception msg)
+            {
+                MessageBox.Show(msg.ToString());
+                throw;
+            }
+        }
+    }
+}
